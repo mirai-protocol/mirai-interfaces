5b8bf318621df8a79e6948310dfdc59596c7cd63
Saurabh Lodha <saurabhlodha221b@gmail.com>
2023-03-29 14:15:25 +0530

    Mirai Interfaces updated code

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mirai Contract Interfaces
+
+* `contracts/IMirai.sol` - Solidity file containing all the interfaces needed for interacting with the core Mirai protocol.
+
+* `abis` - ABIs that can be used to interact with the contracts from off-chain programs.
+* `types` - Types for the ABIS to be imported into application
+* `addresses` - Addresses of deployed Mirai contracts
---
+++ index.d.ts
@@ -0,0 +1,20 @@
+export { default as MiraiABI } from "./abis/Mirai.json"
+export { default as PTokenABI } from "./abis/PToken.json"
+/// Modules
+export { default as DTokenABI } from "./abis/modules/DToken.json"
+export { default as MTokenABI } from "./abis/modules/MToken.json"
+export { default as ExecABI } from "./abis/modules/Exec.json"
+export { default as LiquidationABI } from "./abis/modules/Liquidation.json"
+export { default as MarketsABI } from "./abis/modules/Markets.json"
+export { default as SwapABI } from "./abis/modules/Swap.json"
+
+export type {
+  DToken,
+  MToken,
+  Mirai,
+  PToken,
+  Markets,
+  Liquidation,
+  Swap,
+  MiraiGeneralView,
+} from "./types"
